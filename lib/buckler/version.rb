e0c62eb1fe925a75f65816b7b985658d1d339287
Corey Csuhta <git@cjcsuhta.com>
2016-07-07 12:16:59 -0400

    Version 1.0.2

--- lib/buckler/version.rb
+++ lib/buckler/version.rb
@@ -2,7 +2,7 @@ module Buckler
 
   # Returns Buckler’s version number
   def self.version
-    Gem::Version.new("1.0.1")
+    Gem::Version.new("1.0.2")
   end
 
   # Contains Buckler’s version number
